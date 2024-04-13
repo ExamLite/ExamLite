9476a70d9199b75c46e54c91b277f1595ec78e73
ExamLite <154362176+ExamLite@users.noreply.github.com>
2024-04-13 10:23:25 -0600

    Update 1.1_security_controls.js

--- 1.1_security_controls.js
+++ 1.1_security_controls.js
@@ -202,7 +202,6 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const breakdownElement = document.getElementById("breakdown");
 const answerButtons = document.getElementById("answer-buttons");
 const NextButton = document.getElementById("next-btn");
 
@@ -265,7 +264,6 @@ function selectAnswer(e){
 function showScore(){
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    breakdownElement.innerHTML = "Words words words";
     NextButton.innerHTML = "Take again";
     NextButton.style.display = "block";
 }
